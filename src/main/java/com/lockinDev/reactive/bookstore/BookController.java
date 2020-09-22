b80d44f4bfcf494f57f2b72b377cdf747bb596ff
lockinDev <javieralmario12@gmail.com>
2020-09-22 04:30:30 -0400

    Switched to spring boot platform, removed <<manual>> configuration files. Added application.yml

--- src/main/java/com/lockinDev/reactive/bookstore/BookController.java
+++ src/main/java/com/lockinDev/reactive/bookstore/BookController.java
@@ -4,7 +4,6 @@ package com.lockinDev.reactive.bookstore;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
@@ -29,8 +28,9 @@ public class BookController {
 		return Mono.just("It works");
 	}
 
+	//Test it using:  curl -H "Accept:text/event-stream" http://localhost:8081/book
 	@ResponseStatus(HttpStatus.OK)
-	@GetMapping(path="/books", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+	@GetMapping(path="/books", produces = APPLICATION_STREAM_JSON_VALUE)
 	public Flux<Book> books() {
 		Flux<Book> books = bookRepository.findAll();
 		Flux<Long> periodFlux = Flux.interval(Duration.ofSeconds(2)); // slowing the stream down
---
+++ src/main/java/com/lockinDev/reactive/bookstore/ReactiveApp.java
@@ -0,0 +1,16 @@
+
+package com.lockinDev.reactive.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class ReactiveApp {
+
+	public static void main(String... args) {
+	SpringApplication.run(ReactiveApp.class, args);
+	}
+}
